d643d190ad03ba45edbb6bcbd8c5780f16dbc966
Karuna Hasani <54697582+KarunaKH@users.noreply.github.com>
2021-04-25 09:25:06 -0500

    Update notify.py

--- notify.py
+++ notify.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 import requests as requests
 
 
-def check_availability(zip_codes, date_ranges, radius=5, vac_name="Moderna"):
+def check_availability(zip_codes, date_ranges, radius=4, vac_name="Moderna"):
     found = False
 
     for zip_ in zip_codes:
